4b5abc29727e748107cec90fe467fb782e552f72
Houssam Fathi <ryzenskill@gmail.com>
2023-11-26 03:00:32 +0100

    Made the ui, still have to work on the logic

---
+++ main.js
@@ -0,0 +1,13 @@
+let btnplayer = document.querySelector(".btnplayer");
+let count1 = document.querySelector(".countainer");
+let count2 = document.querySelector(".countainer2")
+
+
+btnplayer.addEventListener("click",()=>{
+count1.style.display = "none";
+count2.style.display = "flex";
+
+})
+
+
+
